4c4d4318b4ff2ec56f12ab7bfce3b28b1a2f2704
YevheniySerhiyenko <johnbellostory2@gmail.com>
2022-02-23 19:23:47 +0200

    fix code smells

--- src/main/java/factory_pattern/Test.java
+++ src/main/java/factory_pattern/Test.java
@@ -1,14 +1,9 @@
 package factory_pattern;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Test {
     public static void main(String[] args) {
+//        write to console developer position, name and age
        Developer developer = DeveloperFactory.createFactory().getDeveloper().setName().setAge();
         System.out.println(developer);
-
-
     }
-
 }
